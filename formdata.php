282f95adc3e15668ee5564f3d200cc54fa87415e
unknown <vilkovchanin@gmail.com>
2023-02-01 13:44:10 +0200

    lesson 24

--- formdata.php
+++ formdata.php
@@ -1,5 +1,12 @@
 <?php
-$a = 5;
-echo $a
+
+$result = mail("vilkovchanin@gmail.com", "Site info", "Info: name $_POST[name]\n Password $_POST[password]\n Save time $_POST[time]\n Sleep $_POST[sleep] hours\n Own tools $_POST[text]\n");
+if ($result){
+    echo "<p>Send is success</p>";
+}
+else {
+    echo "<p>Send is not success</p>";
+}
+
 
 ?>
